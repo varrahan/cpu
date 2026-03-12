f8dda8ba77383c72920616c14f615b454dc492dc
Varrahan <varrahan@gmail.com>
2026-03-12 17:20:51 -0400

    Implement writeback stage

---
+++ rtl/writeback/writeback_stage.v
@@ -0,0 +1,8 @@
+module wb_stage (
+    input  wire [31:0] alu_result,
+    input  wire [31:0] mem_rdata,
+    input  wire        mem_read,
+    output wire [31:0] wb_data
+);
+    assign wb_data = mem_read ? mem_rdata : alu_result;
+endmodule
