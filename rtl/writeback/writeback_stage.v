fee5dcc31385b0c73702e7212ae0aee3fe4488f5
varrahan <varrahan@gmail.com>
2026-08-28 13:21:40 -0400

    Remove stale and unused files

--- rtl/writeback/writeback_stage.v
+++
@@ -1,8 +0,0 @@
-module writeback_stage (
-    input  wire [31:0] alu_result,
-    input  wire [31:0] mem_rdata,
-    input  wire        mem_read,
-    output wire [31:0] wb_data
-);
-    assign wb_data = mem_read ? mem_rdata : alu_result;
-endmodule
