304afb3b2419fe508fc55b891048e75f220c1d83
varrahan <varrahan@gmail.com>
2026-09-02 19:07:36 -0400

    Add top level formal verification

--- formal/safety_harness.sv
+++ formal/safety_harness.sv
@@ -1,10 +1,34 @@
-module safety_harness;
+module regfile_harness;
     (* gclk *) reg clk;
     (* anyseq *) reg rst_n;
 
-    regfile regfile_dut (.clk(clk), .rst_n(rst_n));
-    icache icache_dut (.clk(clk), .rst_n(rst_n));
-    dcache dcache_dut (.clk(clk), .rst_n(rst_n));
-    sv32_mmu mmu_dut (.clk(clk), .rst_n(rst_n));
-    memory_arbiter4 arbiter_dut (.clk(clk), .rst_n(rst_n));
+    regfile dut (.clk(clk), .rst_n(rst_n));
+endmodule
+
+module icache_harness;
+    (* gclk *) reg clk;
+    (* anyseq *) reg rst_n;
+
+    icache dut (.clk(clk), .rst_n(rst_n));
+endmodule
+
+module dcache_harness;
+    (* gclk *) reg clk;
+    (* anyseq *) reg rst_n;
+
+    dcache dut (.clk(clk), .rst_n(rst_n));
+endmodule
+
+module mmu_harness;
+    (* gclk *) reg clk;
+    (* anyseq *) reg rst_n;
+
+    sv32_mmu dut (.clk(clk), .rst_n(rst_n));
+endmodule
+
+module arbiter_harness;
+    (* gclk *) reg clk;
+    (* anyseq *) reg rst_n;
+
+    memory_arbiter4 dut (.clk(clk), .rst_n(rst_n));
 endmodule
